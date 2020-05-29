7629ba39ebf2672748a7a38b3ed285358a11ba65
Leila MARABESE <lemarabe@e1r11p2.42.fr>
2020-05-29 21:44:30 +0200

    can't bash script (file not found) although it is where it's supposed to be

---
+++ srcs/launch_script.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+echo "Je suis dans mon script"
+
+service nginx start
+service php7.3-fpm start
+
+# creer un utilisateur priviligié de la base de données
+
+#while [ TRUE ]; do
+#	echo
+#done
