f2491fbe8cd8617d50ba1902122afff1f432ff27
Leila MARABESE <lemarabe@e1r11p2.42.fr>
2020-05-31 23:49:40 +0200

    pb non resolu, j'ai juste fait des modifs sur openssl req

--- srcs/launch_script.sh
+++ srcs/launch_script.sh
@@ -2,8 +2,7 @@
 
 # creer la cle et le certificat SSL
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -subj /C=FR/ST=IDF/L=Paris/O=42/CN=schene/ \
-    -keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt
+    -keyout /etc/ssl/private/ssl_key.key -out /etc/ssl/certs/ssl_certif.crt
 
 # demarrer les services dont on aura besoin
 service nginx start
